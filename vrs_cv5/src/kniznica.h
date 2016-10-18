2f9e02cb364e233db44bcbdf5fde8ff1f5145282
Jufino <jufo@atlas.sk>
2016-10-18 10:45:01 +0200

    pridanie funkcii do kniznice z nazvom kniznica + pouzitie ADC prerusenia

---
+++ vrs_cv5/src/kniznica.h
@@ -0,0 +1,29 @@
+/*
+ * kniznica.h
+ *
+ *  Created on: 18. 10. 2016
+ *      Author: Juraj
+ */
+
+#ifndef KNIZNICA_H_
+#define KNIZNICA_H_
+/*
+ * kniznica.c
+ *
+ *  Created on: 18. 10. 2016
+ *      Author: Juraj
+ */
+
+#include <stddef.h>
+#include "stm32l1xx.h"
+
+void initLed(void);
+void adc_init(void);
+void initNVIC(void);
+int getADCValue(void);
+void setLedToggle(void);
+void delay(long time);
+
+
+
+#endif /* KNIZNICA_H_ */
